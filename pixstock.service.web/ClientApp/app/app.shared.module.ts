17f08a12b5b107ce8b0c7bec7edacb4605ce8735
atachimiko <atachi.miko@gmail.com>
2018-06-11 00:39:26 +0900

    Add Materials

--- pixstock.service.web/ClientApp/app/app.shared.module.ts
+++ pixstock.service.web/ClientApp/app/app.shared.module.ts
@@ -10,6 +10,8 @@ import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
 
+import 'hammerjs';
+
 @NgModule({
     declarations: [
         AppComponent,
